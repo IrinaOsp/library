eea00663c845936eba95561699c1cfeedce0e996
“Irina <irina.osipova2012@gmail.com>
2023-10-13 14:54:46 +0200

    chore: install prettier

--- library/.eslintrc.cjs
+++ library/.eslintrc.cjs
@@ -1,38 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "airbnb",
-        'airbnb-typescript',
-        "airbnb/hooks",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": './tsconfig.json',
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "react"
-    ],
-    "rules": {
-    }
-}
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-uses-react': 0,
+    '@typescript-eslint/no-explicit-any': 'error',
+    "import/extensions": [
+      "error", "ignorePackages"
+    ]
+  },
+};
--- library/src/App.tsx
+++ library/src/App.tsx
@@ -1,11 +1,7 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
-  return (
-    <div>
-    </div>
-  )
+  return <div>Hello</div>;
 }
 
-export default App
+export default App;
